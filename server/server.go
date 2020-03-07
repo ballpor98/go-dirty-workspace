7d1bc11bcd3eddf5ac98755637066136973f5925
Chawin Kasikitpongpan <chawin.k@kbtg.tech>
2020-03-07 14:38:30 +0700

    restructure file

--- server/server.go
+++ server/server.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/labstack/echo"
 )
 
 func main() {
-	http.HandleFunc("/photos", func(w http.ResponseWriter, r *http.Request) {
+	e := echo.New()
+
+	// Route => handler
+	e.GET("/photos", func(c echo.Context) error {
 		pht := []byte(`{id: 1}`)
-		w.WriteHeader(http.StatusOK)
-		w.Write(pht)
+		return c.JSONBlob(http.StatusOK, pht)
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
 }
